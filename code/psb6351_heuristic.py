eff8091af97ce12e2d1d06ddd548e9516cfaced9
Patricio Viera <pvier002@fiu.edu>
2020-09-22 00:52:03 -0400

    Completed the Heuristic file to the best of ability
    Was able to run the Heudiconv command and change heuristic file aaccordingly - Have not been able to successfully run actual heuristic files

--- code/psb6351_heuristic.py
+++ code/psb6351_heuristic.py
@@ -8,45 +8,25 @@ def create_key(template, outtype=('nii.gz',), annotation_classes=None):
 
 
 def infotodict(seqinfo):
-    """Heuristic evaluator for determining which runs belong where
 
-    allowed template fields - follow python string module:
+    t1w = create_key('C:/Users/Patricio Viera/Documents/GitHub/mattfeld_2020/dset/sub-{subject}/anat/sub-{subject}_T1w')
+    dwi = create_key('C:/Users/Patricio Viera/Documents/GitHub/mattfeld_2020/dset/sub-{subject}/anat/sub-{subject}_run-{item:01d}_dwi')
+    func = create_key('C:/Users/Patricio Viera/Documents/GitHub/mattfeld_2020/dset/sub-{subject}/anat/sub-{subject}_task-REVL_rec-{rec}_run-{item:01d}_bold')
 
-    item: index within category
-    subject: participant id
-    seqitem: run number during scanning
-    subindex: sub index within group
-    """
+    info = {t1w: [], dwi: [], func: []}
 
-    data = create_key('run{item:03d}')
-    info = {data: []}
-    last_run = len(seqinfo)
-
-    for s in seqinfo:
-        """
-        The namedtuple `s` contains the following fields:
-
-        * total_files_till_now
-        * example_dcm_file
-        * series_id
-        * dcm_dir_name
-        * unspecified2
-        * unspecified3
-        * dim1
-        * dim2
-        * dim3
-        * dim4
-        * TR
-        * TE
-        * protocol_name
-        * is_motion_corrected
-        * is_derived
-        * patient_id
-        * study_description
-        * referring_physician_name
-        * series_description
-        * image_type
-        """
-
-        info[data].append(s.series_id)
+    for s in seqinfo:#each row of dicominfo.tsv
+        if (s.dim1 == 256) and (s.dim2 == 256) and (s.dim3 == 176) and (s.dim4 == 1) and ('T1w' in s.protocol_name):
+            info[t1w] = [s.series_id] #This appends to t1w folder if scans meet criteria
+        if (s.dim1 == 140) and (s.dim2 == 140) and (s.dim3 == 81) and (s.dim4 == 103) and ('dMRI' in s.protocol_name):
+            info[dwi] = [s.series_id] #Appends to the dwi folder
+        if (s.dim1 == 100) and (s.dim2 == 100) and (s.dim3 == 66) and (s.dim4 ==355) and ('fMRI' in s.protocol_name): #Appends to the func folder
+            if ('Study_1' in s.protocol_name): #Separates by Study
+                info[func].append({'item': s.series_id, 'rec': 'Study_1'})
+            if ('Study_2' in s.protocol_name)('Study_1' in s.protocol_name):
+                info[func].append({'item': s.series_id, 'rec': 'Study_2'})
+            if ('Study_3' in s.protocol_name):
+                info[func].append({'item': s.series_id, 'rec': 'Study_3'})
+            else:
+                info[func].append({'item': s.series_id, 'rec': 'Study_4'})
     return info
